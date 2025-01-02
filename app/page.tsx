e871334dfab90d9a4931ac1e3d14ffd4c3244170
Isuru Kularathna <isuru.kularathna@novigi.com.au>
2025-01-02 12:52:19 +0530

    setup the project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+
+    <>
+      <p className="font-semibold">Heelo Auth...</p>
+      <Button variant="outline">Click Me</Button>
+    </>
+    
+  );
+}
