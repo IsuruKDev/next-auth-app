5523d17782c725187aa5384c1d625876472d6c08
Isuru Kularathna <isuru.kularathna@novigi.com.au>
2025-01-03 16:05:39 +0530

    develop simple auth page

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,20 @@ export default function Home() {
 
   return (
 
-    <>
-      <p className="font-semibold">Heelo Auth...</p>
-      <Button variant="outline">Click Me</Button>
-    </>
+    <main className="flex h-full flex-col items-center justify-center 
+            bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+              <div className="space-y-6">
+                <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+                🔐 Auth
+                </h1>
+                <p className="text-white text-lg">
+                  A simple authenication service
+                </p>
+                <div>
+                  <Button variant="secondary" size="lg">Sign In</Button>
+                </div>
+              </div>
+    </main>
     
   );
 }
